8e0ed2bea847e33e7c54c2941637122ae7bc3a29
Tulleg <Tulleg@users.noreply.github.com>
2026-02-27 13:06:54 +0100

    initiial

---
+++ markdownConverter.js
@@ -0,0 +1,32 @@
+function convertMarkDown () {
+    const mdInput = document.getElementById(markdown-input);
+    let html = mdInput;
+// headings
+    html = html.replace(/^\s*### (.*$)/gm, '<h3>$1</h3>');
+    html = html.replace(/^\s*## (.*$)/gm, '<h2>$1</h2>');
+    html = html.replace(/^\s*# (.*$)/gm, '<h1>$1</h1>');
+
+// blockquotes
+
+    html = html.replace(/^\s*> (.*$)/gm, '<blockquote>$1</blockquote>')
+
+// images
+    html = html.replace(/!\[(.*?)\]\((.*?)\)/g, '<img alt="$1" src="$2">')
+
+//links
+
+    html = html.replace(/\[(.*?)\]\((.*?)\)/g, '<a href="$2">$1</a>')
+
+// fett
+    html = html.replace(/(\*\*|__)(.*?)\1/g, '<strong>$2</strong>')
+// kursiv
+    html = html.replace(/(\*|_)(.*?)\1/g, '<em>$2</em>')
+// Zeilenumbrüche
+    html = html.replace(/\n/g, '<br>')
+    
+
+
+
+
+
+}
